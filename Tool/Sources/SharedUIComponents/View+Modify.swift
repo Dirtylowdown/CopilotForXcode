f22e12749be8187f1ede69e302e30d2a4f6703d7
Dirtylowdown <cactusjacknf@gmail.com>
2025-02-05 23:21:36 -0600

    Update View+Modify.swift

--- Tool/Sources/SharedUIComponents/View+Modify.swift
+++ Tool/Sources/SharedUIComponents/View+Modify.swift
@@ -1,10 +1,5 @@
-import SwiftUI
-
-public extension View {
-    @ViewBuilder func modify<Content: View>(@ViewBuilder transform: (Self) -> Content)
-        -> some View
-    {
-        transform(self)
-    }
-}
-
+uninstall
+delete
+stop
+close
+delete
