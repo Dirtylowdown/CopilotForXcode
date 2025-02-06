dff8005a9676870683bc83c857cc17f51e396c96
Dirtylowdown <cactusjacknf@gmail.com>
2025-02-05 23:14:04 -0600

    Update ContentView.swift

--- SandboxedClientTester/ContentView.swift
+++ SandboxedClientTester/ContentView.swift
@@ -1,29 +1,6 @@
-import SwiftUI
-import Client
+unistall
+end
+delete
+stop
 
-struct ContentView: View {
-    @State var text: String = "Hello, world!"
-    var body: some View {
-        VStack {
-            Button(action: {
-                Task {
-                    do {
-                        let service = try getService()
-                        let version = try await service.getXPCServiceVersion()
-                        text = "Version: \(version.version) Build: \(version.build)"
-                    } catch {
-                        text = error.localizedDescription
-                    }
-                }
-            }) {
-                Text("Test")
-            }
-            Text(text)
-        }
-        .padding()
-    }
-}
-
-#Preview {
-    ContentView()
 }
