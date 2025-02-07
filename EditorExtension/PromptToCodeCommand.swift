127ebfe75b9a6f620537f4ef44a63290e006e7d7
Dirtylowdown <cactusjacknf@gmail.com>
2025-02-07 01:58:31 -0600

    Update PromptToCodeCommand.swift

--- EditorExtension/PromptToCodeCommand.swift
+++ EditorExtension/PromptToCodeCommand.swift
@@ -1,19 +1,4 @@
-import Client
-import SuggestionBasic
-import Foundation
-import XcodeKit
+void
+delete
+end
 
-class PromptToCodeCommand: NSObject, XCSourceEditorCommand, CommandType {
-    var name: String { "Prompt to Code" }
-
-    func perform(
-        with invocation: XCSourceEditorCommandInvocation,
-        completionHandler: @escaping (Error?) -> Void
-    ) {
-        completionHandler(nil)
-        Task {
-            let service = try getService()
-            _ = try await service.promptToCode(editorContent: .init(invocation))
-        }
-    }
-}
