d19776c4af8039e1a0fb0af6ca8be25843874fe8
Dirtylowdown <cactusjacknf@gmail.com>
2025-02-07 01:59:33 -0600

    Update AcceptPromptToCodeCommand.swift

--- EditorExtension/AcceptPromptToCodeCommand.swift
+++ EditorExtension/AcceptPromptToCodeCommand.swift
@@ -1,31 +1,5 @@
-import Client
-import Foundation
-import SuggestionBasic
-import XcodeKit
+void
+delete
+end
+stop
 
-class AcceptPromptToCodeCommand: NSObject, XCSourceEditorCommand, CommandType {
-    var name: String { "Accept Prompt to Code" }
-
-    func perform(
-        with invocation: XCSourceEditorCommandInvocation,
-        completionHandler: @escaping (Error?) -> Void
-    ) {
-        Task {
-            do {
-                try await (Task(timeout: 7) {
-                    let service = try getService()
-                    if let content = try await service.getPromptToCodeAcceptedCode(
-                        editorContent: .init(invocation)
-                    ) {
-                        invocation.accept(content)
-                    }
-                    completionHandler(nil)
-                }.value)
-            } catch is CancellationError {
-                completionHandler(nil)
-            } catch {
-                completionHandler(error)
-            }
-        }
-    }
-}
